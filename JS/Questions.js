22186dc9d6b880d02ef245caddbe9a778aef250d
BipinNeupane <bipin2022121@nami.edu.np>
2022-08-06 21:34:31 +0545

    final commit

---
+++ JS/Questions.js
@@ -0,0 +1,74 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What is the full form of JS?",
+  answer: "Java Script",
+  options: [
+    "Java Security",
+    "Java Script",
+    "Jackson Smith",
+    "Joint Server"
+  ]
+},
+  {
+  numb: 2,
+  question: "HexaDecimal Number System has the base of?",
+  answer: "16",
+  options: [
+    "2",
+    "8",
+    "16",
+    "32"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which is the current Intel generation?",
+  answer: "12",
+  options: [
+    "7",
+    "10",
+    "12",
+    "9"
+  ]
+},
+  {
+  numb: 4,
+  question: "Which of the following is volatile?",
+  answer: "RAM",
+  options: [
+    "ROM",
+    "NVRAM",
+    "RAM",
+    "Motherboard"
+  ]
+},
+  {
+  numb: 5,
+  question: "JAVA is known as?",
+  answer: "Object-Oriented Programming Language",
+  options: [
+    "HyperText Markup Language",
+    "Object-Oriented Programming Language",
+    "Logic Programming Language",
+    "Scripting Programming Language"
+  ]
+},
+
+
+
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
